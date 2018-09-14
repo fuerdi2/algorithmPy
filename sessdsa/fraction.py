afeb805eb78b4875a9946b241d6e72e795dd7f72
Left <fuerdi2@163.com>
2018-09-14 13:20:23 +0800

    fraction.py

---
+++ sessdsa/fraction.py
@@ -0,0 +1,21 @@
+class Fraction:
+    #the methods go here
+    def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
+    def show(self):
+        print(self.num,"/",self.den)
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+    def __add__(self,otherfraction):
+        newnum = self.num*otherfraction.den+self.den*otherfraction.num
+        newden = self.den*otherfraction.den
+        return Fraction(newnum,newden)
+myfraction = Fraction(3,5)
+myfraction.show()
+print(myfraction)
+f1 = Fraction(1,4)
+f2 = Fraction(1,2)
+f3 = f1 + f2
+print(f3)
+
